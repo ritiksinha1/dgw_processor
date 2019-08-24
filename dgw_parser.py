c0bca06c4a396e3093b458a7ca9e6191e0cd1977
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-08-24 12:42:41 -0400

    Update dgw_parser.py and requirements.py
    Work in Progress

--- dgw_parser.py
+++ dgw_parser.py
@@ -4,100 +4,66 @@ import argparse
 import psycopg2
 from psycopg2.extras import NamedTupleCursor
 
-import re
-from requirements import Requirements
-
-
-def parse_catalog_years(period_start, period_stop):
-  """ First Year = CCYY-YY or CCYY-CCYY if centuries differ
-      Last Year = CCYY-YY or CCYY-CCYY or 'Now'
-      Other values: 'Missing', 'Unknown', or 'Unused'
-      Catalogs: list which will be either empty, Undergraduate, Graduate, or both
-
-  """
-  catalogs = set()
-  first_academic_year = ''
-  last_academic_year = ''
-
-  m_start = re.search(r'(19|20)(\d\d)-?(19|20)(\d\d)([UG]?)', period_start)
-  if m_start is None:
-    first_academic_year = 'Unknown'
-  else:
-    century_1, year_1, century_2, year_2, catalog = m_start.groups()
-    if century_1 != century_2:
-      first_academic_year = f'{century_1}{year_1}-{century_2}{year_2}'
-    else:
-      first_academic_year = f'{century_1}{year_1}-{year_2}'
-    if catalog == 'U':
-      catalogs.add('Undergraduate')
-    if catalog == 'G':
-      catalogs.add('Graduate')
-
-  if re.search(r'9999+', period_stop):
-    last_academic_year = 'Now'
-  else:
-    m_stop = re.search(r'(19|20)(\d\d)-?(19|20)(\d\d)([UG]?)', period_stop)
-    if m_stop is None:
-      last_academic_year = 'Unknown'
-    else:
-      century_1, year_1, century_2, year_2, catalog = m_stop.groups()
-      if century_1 != century_2:
-        last_academic_year = f'{century_1}{year_1}-{century_2}{year_2}'
-      else:
-        last_academic_year = f'{century_1}{year_1}-{year_2}'
-      if catalog == 'U':
-        catalogs.add('Undergraduate')
-      if catalog == 'G':
-        catalogs.add('Graduate')
-
-  if first_academic_year != last_academic_year:
-    return f'{first_academic_year} to {last_academic_year}', catalogs
-  else:
-    return f'{first_academic_year}', catalogs
-
-
-def parse_requirements(requirement_text):
-  """ Return Requirements object
-      This is going to be a class with a json member and a __str__() method.
-  """
-  requirements = Requirements(requirement_text)
-  return requirements.html()
+from requirements import Requirements, parse_catalog_years
 
 
 # Unit test
 if __name__ == '__main__':
+  # Set up parser
   parser = argparse.ArgumentParser(description='Test DGW Parser')
-  parser.add_argument('programs', nargs='+')
-  parser.add_argument('-i', '--institution', default='QNS01')
+  parser.add_argument('-p', '--programs', nargs='+')
+  parser.add_argument('-i', '--institutions', nargs='*')
   parser.add_argument('-d', '--debug', action='store_true', default=False)
   parser.add_argument('-v', '--verbose', action='store_true', default=False)
+
+  # Parse args and handle default list of institutions
   args = parser.parse_args()
+  if args.institutions is None:
+    institutions = ['QNS01']
+  else:
+    institutions = args.institutions
+  print('programs:', args.programs)
+  print('institutions:', institutions)
 
-  # Put arguments in canonical form
+  # Create dict of known colleges
+  colleges = dict()
+  crse_conn = psycopg2.connect('dbname=cuny_courses')
+  crse_cursor = crse_conn.cursor(cursor_factory=NamedTupleCursor)
+  crse_cursor.execute('select substr(lower(code),0,4) as code, name from institutions')
+  for row in crse_cursor.fetchall():
+    colleges[row.code] = row.name
+  crse_conn.close()
+
+  # Set up for querying program information
+  prog_conn = psycopg2.connect('dbname=cuny_programs')
+  prog_cursor = prog_conn.cursor(cursor_factory=NamedTupleCursor)
   query_base = 'select requirement_text from requirement_blocks where '
-  institution = f'{args.institution.lower()[0:3]}'
-
-  type_list = ['major1 = %s', 'major2 = %s', 'minor = %s', 'concentration = %s']
+  type_list = ['major1 ~* %s', 'major2 ~* %s', 'minor ~* %s', 'concentration ~* %s']
   type_clause = f" and ({' or '.join(type_list)})"
 
-  conn = psycopg2.connect('dbname=cuny_programs')
-  cursor = conn.cursor(cursor_factory=NamedTupleCursor)
-  for program in args.programs:
-    program_code = program.upper()
-    types = [f'{program}'] * type_clause.count('%s')
-    query = f"""
-              select * from requirement_blocks
-              where institution = '{institution}'
-              {type_clause}
-              order by period_start desc
-            """
-    cursor.execute(query, types)
-    for row in cursor.fetchall():
-      request_block = {}
-      request_block['institution'] = row.institution
-      (request_block['catalog_years'],
-       request_block['catalogs']) = parse_catalog_years(row.period_start, row.period_stop)
-      request_block['program_type'] = row.block_type
-      request_block['program'] = row.block_value
-      request_block['requirements'] = parse_requirements(row.requirement_text)
-      print(request_block)
+  # Go through the selected programs for each college
+  for college_code in colleges.keys():
+    college_name = colleges[college_code]
+    for program in args.programs:
+      program_code = program.upper()
+      types = [f'{program}'] * type_clause.count('%s')
+      query = f"""
+                select * from requirement_blocks
+                where institution = '{college_code}'
+                {type_clause}
+                order by period_start desc
+              """
+      prog_cursor.execute(query, types)
+      for row in prog_cursor.fetchall():
+        requirement_block = {}
+        requirement_block['college'] = college_name
+        (requirement_block['catalog_years'],
+         requirement_block['catalogs']) = parse_catalog_years(row.period_start, row.period_stop)
+        requirement_block['program_type'] = row.block_type
+        requirement_block['program'] = row.block_value
+        requirement_block['requirements'] = Requirements(row.requirement_text).html()
+        print(requirement_block['college'],
+              requirement_block['program'], row.title,
+              requirement_block['catalog_years'],
+              requirement_block['catalogs'])
+        print(requirement_block['requirements'])
--- requirements.py
+++ requirements.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 
 class Requirements():
@@ -6,6 +7,16 @@ class Requirements():
       The constructor takes a text description in Degreeworks Scribe format, which is stored as the
       "scribe_text" member. The parsed information is available in the "json" and "html" members.
       The _str_() method returns a plain text version.
+
+      Fields:
+        code    Program Code from CUNYfirst
+        title   Program Title
+        years   Catalog Year(s)
+        credits Total Credits
+        limit   Transfer Credits Allowed
+        courses Required Courses
+        notes   Notes
+
   """
 
   def __init__(this, requirement_text):
@@ -28,3 +39,74 @@ class Requirements():
     return f"""<p>A total of {this.requirements['total_credits']} credits.</p>
             """
 
+
+class AcademicYear:
+  """
+  """
+  def __init__(self, century_1=None, year_1=None, century_2=None, year_2=None):
+    """ Academic_Year constructor. Second year must be one greater than the first.
+        Omit args for “Now”
+    """
+    if century_1 is None:
+      self.is_now = True
+    else:
+      self.is_now = False
+      self.century_1 = int(century_1)
+      self.year_1 = int(year_1)
+      self.century_2 = int(century_2)
+      self.year_2 = int(year_2)
+      if (100 * self.century_1 + self.year_1 + 1) != (100 * self.century_2 + self.year_2):
+        raise ValueError(f'{100 * self.century_1 + self.year_1}, '
+                         f'{100 * self.century_2 + self.year_2} is not a valid pair of years')
+
+  def __str__(self):
+    if self.is_now:
+      return 'Now'
+    else:
+      if self.century_1 != self.century_2:
+        return f'{self.century_1}{self.year_1:02}-{self.century_2}{self.year_2:02}'
+      else:
+        return f'{self.century_1}{self.year_1:02}-{self.year_2:02}'
+
+
+class Catalogs:
+  def __init__(self, period_start, period_stop):
+    """ First Year = CCYY-YY or CCYY-CCYY if centuries differ
+        Last Year = CCYY-YY or CCYY-CCYY or 'Now'
+        Other values: 'Missing', 'Unknown', or 'Unused'
+        Catalogs: list which will be either empty, Undergraduate, Graduate, or both
+
+    """
+    self.which_catalogs = set()
+    self.first_academic_year = None
+    self.last_academic_year = None
+
+    m_start = re.search(r'(19|20)(\d\d)-?(19|20)(\d\d)([UG]?)', period_start)
+    if m_start is not None:
+      century_1, year_1, century_2, year_2, catalog = m_start.groups()
+      self.first_academic_year = AcademicYear(century_1, year_1, century_2, year_2)
+      if catalog == 'U':
+        self.which_catalogs.add('Undergraduate')
+      if catalog == 'G':
+        self.which_catalogs.add('Graduate')
+
+    if re.search(r'9999+', period_stop):
+      self.last_academic_year = AcademicYear(None, None, None, None)
+    else:
+      m_stop = re.search(r'(19|20)(\d\d)-?(19|20)(\d\d)([UG]?)', period_stop)
+      if m_stop is not None:
+        century_1, year_1, century_2, year_2, catalog = m_stop.groups()
+        assert (int(century_2) * 100 + int(year_2)) == (1 + int(century_1) * 100 + int(year_1))
+        if century_1 != century_2:
+          last_academic_year = f'{century_1}{year_1}-{century_2}{year_2}'
+        else:
+          last_academic_year = f'{century_1}{year_1}-{year_2}'
+        if catalog == 'U':
+          catalogs.add('Undergraduate')
+        if catalog == 'G':
+          catalogs.add('Graduate')
+
+    if first_academic_year != last_academic_year:
+      return f'{first_academic_year} to {last_academic_year}', catalogs
+    else:
+      return f'{first_academic_year}', catalogs
