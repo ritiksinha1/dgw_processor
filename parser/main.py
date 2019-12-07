07c9727a65f2bb9c1b0d1f0f0d1f325f45e13b39
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-12-06 23:03:19 -0500

    Update main.py
    Working with course lists.

--- parser/main.py
+++ parser/main.py
@@ -2,6 +2,7 @@
 
 import inspect
 from pprint import pprint
+from typing import List, Set, Dict, Tuple, Optional
 
 import argparse
 import sys
@@ -9,6 +10,7 @@ from io import StringIO
 
 import psycopg2
 from psycopg2.extras import NamedTupleCursor
+from collections import namedtuple
 
 from antlr4 import *
 from ReqBlockLexer import ReqBlockLexer
@@ -21,8 +23,19 @@ for c in range(13, 31):
 
 trans_table = str.maketrans(trans_dict)
 
+CourseInfo = namedtuple('CourseInfo', 'institution course_id offer_nbr discipline catalog_number '
+                        'title course_status designation attributes')
 
-def classes_or_credits(ctx):
+# Create dict of known colleges
+colleges = dict()
+course_conn = psycopg2.connect('dbname=cuny_courses')
+course_cursor = course_conn.cursor(cursor_factory=NamedTupleCursor)
+course_cursor.execute('select substr(lower(code),0,4) as code, name from institutions')
+for row in course_cursor.fetchall():
+  colleges[row.code] = row.name
+
+
+def classes_or_credits(ctx) -> str:
   """
   """
   classes_credits = ctx.CREDITS()
@@ -31,22 +44,66 @@ def classes_or_credits(ctx):
   return str(classes_credits).lower()
 
 
-def build_course_list(ctx):
-  """ INFROM? (SYMBOL | WILDSYMBOL) (NUMBER | RANGE | WILDNUMBER) (AND  ((SYMBOL NUMBER) | NUMBER))*
-      INFROM? (SYMBOL | WILDSYMBOL) (NUMBER | RANGE | WILDNUMBER) (OR  ((SYMBOL NUMBER) | NUMBER))*
+def build_course_list(ctx) -> List[Dict]:
+  """ INFROM? class_item (AND class_item)*
+      INFROM? class_item (OR class_item)*
   """
+  print(f'build_course_list({ctx})')
+  course_list = []
   if ctx is None:
-    return
-  print(ctx.INFROM())
-  print(ctx.SYMBOL(), ctx.WILDSYMBOL(),)
-  print(ctx.NUMBER(), ctx.RANGE(), ctx.WILDNUMBER())
+    return course_list
+  # if ctx.INFROM():
+  #   print(f'INFROM: {ctx.INFROM()}')
+  for class_item in ctx.class_item():
+    if class_item.SYMBOL():
+      display_discipline = str(class_item.SYMBOL())
+      search_discipline = display_discipline
+    if class_item.WILDSYMBOL():
+      display_discipline = str(class_item.WILDSYMBOL())
+      search_discipline = display_discipline.replace('@', '.*')
+    if class_item.NUMBER():
+      display_number = str(class_item.NUMBER())
+      search_number = display_number
+    if class_item.RANGE():
+      display_number = str(class_item.RANGE())
+      low, high = display_number.split(':')
+      search_number = f'>= {low}\' and catalog_number <= \'{high}'
+    if class_item.WILDNUMBER():
+      display_number = str(class_item.wildnumber)
+      search_number = display_number.replace('@', '.*')
+    print(display_discipline, display_number)
+    course_query = f"""
+                      select institution, course_id, offer_nbr, discipline, catalog_number, title,
+                             course_status, designation, attributes
+                        from courses
+                       where institution ~* '{institution}'
+                         and discipline ~ '{search_discipline}'
+                         and catalog_number ~ '{search_number}'
+                    """
+    course_cursor.execute(course_query)
+    details = []
+    details = [CourseInfo._Make(row) for row in cursor.fetchall()]
+    course_list.append({'display': f'{display_discipline} {display_number}',
+                        'info': tuple(details)})
+  pprint(course_list)
+  return course_list
+
+
+def course_list_to_html(courses: List[str]):
+  html = ''
+  for course in courses:
+    html += '<p>course</p>'
 
 
 class ReqBlockInterpreter(ReqBlockListener):
-  def __init__(self, block_type, block_value, title):
+  def __init__(self, institution, block_type, block_value, title):
+    self.institution = institution
     self.block_type = block_type.lower()
     self.block_value = block_value
     self.title = title
+    college_name = colleges[institution]
+    self.html = f"""<h1>{college_name} {self.title}</h1>
+                 """
     if self.block_type == 'conc':
       self.block_type = 'concentration'
 
@@ -57,21 +114,39 @@ class ReqBlockInterpreter(ReqBlockListener):
     # print(inspect.getmembers(ctx))
     if float(str(ctx.NUMBER())) == 1:
       classes_credits = classes_credits.strip('es')
-    print(f'At least {ctx.NUMBER()} {str(classes_credits).lower()} must be completed in residency.')
+    self.html += (f'<p>At least {ctx.NUMBER()} {str(classes_credits).lower()} '
+                  f'must be completed in residency.</p>')
 
   def enterNumcredits(self, ctx):
     """ NUMBER CREDITS (and_courses | or_courses)?
     """
-    print('and_courses', ctx.and_courses())
-    print('or_courses: ', ctx.or_courses())
-    print(f'This {self.block_type} requires {ctx.NUMBER()} credits.')
+    self.html += (f'<p>This {self.block_type} requires {ctx.NUMBER()} credits.')
+    if ctx.and_courses() is not None:
+      build_course_list(ctx.and_courses())
+    if ctx.or_courses() is not None:
+      self.html += course_list_to_html(build_course_list(ctx.or_courses()))
 
   def enterMaxcredits(self, ctx):
     """ MAXCREDITS NUMBER (and_courses | or_courses)
     """
-    build_course_list(ctx.and_courses())
-    build_course_list(ctx.or_courses())
-    print('enterMaxcredits() here', ctx.NUMBER())
+    print('enterMaxcredits() here')
+    print(f'MAXCREDITS: {ctx.MAXCREDITS()}')
+    print(f'NUMBER: {ctx.NUMBER()}')
+    if ctx.and_courses() is not None:
+      build_course_list(ctx.and_courses())
+    if ctx.or_courses() is not None:
+      build_course_list(ctx.or_courses())
+
+  def enterMaxclasses(self, ctx):
+    """ MAXCLASSES NUMBER (and_courses | or_courses)
+    """
+    print('enterMaxclasses() here')
+    print(f'MAXCLASSES: {ctx.MAXCLASSES()}')
+    print(f'NUMBER: {ctx.NUMBER()}')
+    if ctx.and_courses() is not None:
+      build_course_list(ctx.and_courses())
+    if ctx.or_courses() is not None:
+      build_course_list(ctx.or_courses())
 
   def enterMaxpassfail(self, ctx):
     """ MAXPASSFAIL NUMBER (CREDITS | CLASSES) (TAG '=' SYMBOL)?
@@ -79,19 +154,20 @@ class ReqBlockInterpreter(ReqBlockListener):
     print('enterMaxpassfail() here')
 
 
-# main()
+# dgw_parser()
 # -------------------------------------------------------------------------------------------------
-def main(req_text, block_type, block_value, title):
+def dgw_parser(institution, req_text, block_type, block_value, title):
   """  Mainly, this is the main testing thing.
   """
   input_stream = InputStream(req_text)
   lexer = ReqBlockLexer(input_stream)
   token_stream = CommonTokenStream(lexer)
   parser = ReqBlockParser(token_stream)
-  interpreter = ReqBlockInterpreter(block_type, block_value, title)
+  interpreter = ReqBlockInterpreter(institution, block_type, block_value, title)
   walker = ParseTreeWalker()
   tree = parser.req_text()
   walker.walk(interpreter, tree)
+  print(interpreter.html)
 
 
 if __name__ == '__main__':
@@ -115,15 +191,6 @@ if __name__ == '__main__':
     print(f'institutions: {institutions}')
     print(f'types: {types}')
     print(f'values: {values}')
-
-  # Create dict of known colleges
-  colleges = dict()
-  course_conn = psycopg2.connect('dbname=cuny_courses')
-  course_cursor = course_conn.cursor(cursor_factory=NamedTupleCursor)
-  course_cursor.execute('select substr(lower(code),0,4) as code, name from institutions')
-  for row in course_cursor.fetchall():
-    colleges[row.code] = row.name
-  course_conn.close()
 
   # Get the top-level requirements to examine: college, block-type, and/or block value
   conn = psycopg2.connect('dbname=cuny_programs')
@@ -151,4 +218,4 @@ if __name__ == '__main__':
                                   .strip('"')\
                                   .replace('\\r', '\r')\
                                   .replace('\\n', '\n')
-            main(requirement_text, type, value, row.title)
+            dgw_parser(institution, requirement_text + '\n', type, value, row.title)
