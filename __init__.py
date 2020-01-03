e96e6e2e59f03e3ccf3c70bb7418c97900da9689
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-01-03 09:47:10 -0500

    Update __init__.py
    Add docstring and version string; update __all__ to limit what can be
    imported to just dgw_parser.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-__all__ = ['dgw_parser', 'ReqBlockLexer', 'ReqBlockParser', 'ReqBlockListener']
+""" Parse Degreeworks Scribe Blocks.
+"""
+
+# The only module importable from this package.
+__all__ = ['dgw_parser']
+__version__ = '0.0.2'
