b3d49f0855dccc20f3180fd79758e324513718d3
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-01-03 23:38:35 -0500

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as readme:
   long_description = readme.read()
 
 setup(name='dgw_processor',
-      version='0.0.8',
+      version='0.0.9',
       author='Christopher Vickery',
       author_email='christopher.vickery@qc.cuny.edu',
       description='Process Degreeworks Scribe Blocks',
