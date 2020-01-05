9d89234aba250626d55646fadc1b496483ba4104
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-01-04 19:51:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,23 @@ from setuptools import setup, find_packages
 with open('README.md', 'r') as readme:
   long_description = readme.read()
 
-setup(name='dgw_processor',
-      version='0.0.10',
-      author='Christopher Vickery',
-      author_email='christopher.vickery@qc.cuny.edu',
-      description='Process Degreeworks Scribe Blocks',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='https://github.com/cvickery/dgw_processor',
-      packages=find_packages(),
-      classifiers=['Programming Language :: Python :: 3',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   ],
-      python_requires='>= 3.6',
-      )
+setup(
+    author='Christopher Vickery',
+    author_email='christopher.vickery@qc.cuny.edu',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    description='Process Degreeworks Scribe Blocks',
+    include_package_data=True,
+    keywords='dgw_processor',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name='dgw_processor',
+    packages=find_packages(include=['dgw_processor', 'dgw_processor.*']),
+    python_requires='>= 3.6',
+    url='https://github.com/cvickery/dgw_processor',
+    version='0.0.10',
+    zip_safe=False,
+)
