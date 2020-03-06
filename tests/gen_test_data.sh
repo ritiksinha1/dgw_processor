513300716fbb00ec6f875d3fa119caeac1e2371d
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-03-06 11:42:47 -0500

    Update gen_test_data.sh
    Take institution, type, and value as optional command line args and
    produce structured filename for output.

--- tests/gen_test_data.sh
+++ tests/gen_test_data.sh
@@ -1,18 +1,41 @@
 #! /usr/local/bin/bash
 
 # Extract requirement_text from the db.
-# Get the institution from the environment, but get the block_value from the command line.
 
-institution=$INSTITUTION
-[[ -z $institution ]] && institution='qns'
 
+# Defaults
+institution=QNS
+type=MAJOR
 value=CSCI-BA
-if [[ $# -gt 0 ]]
-then value=$1
-fi
 
-psql cuny_programs -Xqtc "select requirement_text \
+while [[ $# > 0 ]]
+do
+  case $1 in
+    -i) institution=$2
+        shift
+        ;;
+    -t) type=$2
+        shift
+        ;;
+    -v) value=$2
+        shift
+        ;;
+     *) echo "usage: gen_test_data.sh [-i institution] [-t type] [-v value]"
+        exit 1
+        ;;
+  esac
+  shift
+done
+
+# DEBUG:
+# echo $institution
+# echo $type
+# echo $value
+outfile=`echo "./test_data/${institution}_${type}_${value}.txt" | tr A-Z a-z`
+echo Generating $outfile
+psql cuny_curriculum -Xqtc "select requirement_text \
 from requirement_blocks
-where institution = '$institution'
-and block_value = '$value'
-and period_stop = '99999999'" | sed s/\ *+$// |sed s/\\\\r//
+where institution ~* '$institution'
+and block_type ~* '$type'
+and block_value ~* '$value'
+and period_stop = '99999999'"  | sed s/\ *+$// |sed s/\\\\r// > $outfile
