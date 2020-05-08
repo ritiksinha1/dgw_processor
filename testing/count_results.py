2da2df2be085d7d50a22f98efd616a3a3daacd62
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-05-08 13:49:30 -0400

    Update testing
    More automation; better formatting

--- testing/count_results.py
+++ testing/count_results.py
@@ -2,20 +2,29 @@
 """ Count the sizes of the test_result files.
     Report all sizes after telling what percentage were length 0, indicating complete parsing.
 """
+from datetime import datetime
 from pathlib import Path
 from collections import Counter, defaultdict
-all_sizes = Counter()
-school_sizes = defaultdict(Counter)
-for file in Path('test_results').glob('*'):
-  size = file.stat().st_size
-  all_sizes[size] += 1
-  school = file.name[0:3]
-  school_sizes[school][size] += 1
 
-print(f'{100 * all_sizes[0] / sum(all_sizes.values()):.1f}%')
-for key in sorted(all_sizes.keys()):
-  print(f'{key:04}: {all_sizes[key]:,}')
+time_stamp = str(datetime.now()).replace(' ', '_').rstrip('0123456789').rstrip('.')
+with open(f'count_results_{time_stamp}.log', 'w') as logfile:
+  for block_type in ['major', 'minor', 'conc', 'degree']:
+    block_type_str = block_type.upper()
+    num_files = len([f for f in Path(f'test_results.{block_type}').glob('*')])
+    all_sizes = Counter()
+    school_sizes = defaultdict(Counter)
+    for file in Path(f'test_results.{block_type}').glob('*'):
+      size = file.stat().st_size
+      all_sizes[size] += 1
+      school = file.name[0:3]
+      school_sizes[school][size] += 1
 
-for school in sorted(school_sizes.keys()):
-  print(f'{school}: {100 * school_sizes[school][0] / sum(school_sizes[school].values()):>5.1f}%')
+    print(f'ALL: {100 * all_sizes[0] / sum(all_sizes.values()):.1f}% of '
+          f'{num_files:,} {block_type_str}s', file=logfile)
+    # for key in sorted(all_sizes.keys()):
+    #   print(f'{key:04}: {all_sizes[key]:,}')
+
+    for school in sorted(school_sizes.keys()):
+      print(f'{school}: {100 * school_sizes[school][0] / sum(school_sizes[school].values()):>5.1f}%'
+            f' of {sum(school_sizes[school].values()):,} {block_type_str.lower()}s', file=logfile)
 
--- testing/gen_test_data.py
+++ testing/gen_test_data.py
@@ -1,21 +1,45 @@
 #! /usr/local/bin/python3
 """ Look up the requirement blocks for all active majors and save them in the test_data directory.
 """
+import argparse
+import re
+import sys
+
 from pathlib import Path
 from pgconnection import PgConnection
-import re
+
+parser = argparse.ArgumentParser(description='Test DGW Parser')
+parser.add_argument('-d', '--debug', action='store_true', default=False)
+parser.add_argument('-t', '--block_type', default='MAJOR')
+parser.add_argument('-p', '--period', default='99999999')
+# Parse args and handle default list of institutions
+args = parser.parse_args()
+block_type = args.block_type.upper()
+period_stop = args.period
+if period_stop.lower() == 'active':
+  period_stop = '99999999'
+if period_stop != '99999999':
+  exit(f'"{period_stop}" is an unsupported period. '
+       'Only "active" or "99999999" are valid for now.')
+
+directory = Path(f'test_data.{block_type.lower()}')
+try:
+  directory.mkdir(mode=0o755, exist_ok=True)
+except FileExistsError as fee:
+  exit(f'{directory} is not a directory')
 
 conn = PgConnection()
 cursor = conn.cursor()
 
-cursor.execute('select * from requirement_blocks order by institution, requirement_id')
-print(cursor.rowcount)
-count = 0
+cursor.execute(f"""
+               select * from requirement_blocks
+               where block_type = '{block_type}'
+                 and period_stop = '{period_stop}'
+                 order by institution, requirement_id""")
+print(f'{cursor.rowcount} {block_type}s found')
+
 for block in cursor.fetchall():
-  if block.block_type == 'MAJOR' and block.period_stop == '99999999':
-    count += 1
-    title_str = re.sub(r'\s$', '', re.sub(r'_+', '_', re.sub(r'[\][\(\):/\& ]', '_', block.title)))
-    file = Path('test_data/',
-                f'{block.institution}_{block.requirement_id}_{title_str}')
-    file.write_text(re.sub(r'[Ee][Nn][Dd]\.(.|\n)*', 'END.\n', block.requirement_text))
-print(count)
+  title_str = re.sub(r'\s$', '', re.sub(r'_+', '_', re.sub(r'[\][\(\):/\& ]', '_', block.title)))
+  file = Path(directory,
+              f'{block.institution}_{block.requirement_id}_{title_str}'.strip('_'))
+  file.write_text(re.sub(r'[Ee][Nn][Dd]\.(.|\n)*', 'END.\n', block.requirement_text))
