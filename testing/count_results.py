4c739385509f7bcb4dc699c8046d7dd2306aaefd
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-05-07 23:13:23 -0400

    Update testing utils
    Generate better file names for req bocks.

--- testing/count_results.py
+++ testing/count_results.py
@@ -3,19 +3,19 @@
     Report all sizes after telling what percentage were length 0, indicating complete parsing.
 """
 from pathlib import Path
-from collections import Counter
+from collections import Counter, defaultdict
 all_sizes = Counter()
-school_sizes = Counter()
+school_sizes = defaultdict(Counter)
 for file in Path('test_results').glob('*'):
   size = file.stat().st_size
-  all_sizes[file.stat().st_size] += 1
+  all_sizes[size] += 1
   school = file.name[0:3]
-  school_sizes[f'{school}-{size}'] += 1
+  school_sizes[school][size] += 1
 
 print(f'{100 * all_sizes[0] / sum(all_sizes.values()):.1f}%')
 for key in sorted(all_sizes.keys()):
   print(f'{key:04}: {all_sizes[key]:,}')
 
-for key in sorted(school_sizes.keys()):
-  print(f'{key}: {school_sizes[key]:,}')
+for school in sorted(school_sizes.keys()):
+  print(f'{school}: {100 * school_sizes[school][0] / sum(school_sizes[school].values()):>5.1f}%')
 
--- testing/gen_test_data.py
+++ testing/gen_test_data.py
@@ -3,6 +3,7 @@
 """
 from pathlib import Path
 from pgconnection import PgConnection
+import re
 
 conn = PgConnection()
 cursor = conn.cursor()
@@ -13,8 +14,8 @@ count = 0
 for block in cursor.fetchall():
   if block.block_type == 'MAJOR' and block.period_stop == '99999999':
     count += 1
-    title_str = block.title.replace('/', '-').replace(':', '-').replace(' ', '_')
+    title_str = re.sub(r'\s$', '', re.sub(r'_+', '_', re.sub(r'[\][\(\):/\& ]', '_', block.title)))
     file = Path('test_data/',
                 f'{block.institution}_{block.requirement_id}_{title_str}')
-    file.write_text(block.requirement_text)
+    file.write_text(re.sub(r'[Ee][Nn][Dd]\.(.|\n)*', 'END.\n', block.requirement_text))
 print(count)
