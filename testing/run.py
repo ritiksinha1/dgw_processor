16d88762885e18593f3691d59101439d2f3a7573
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-05-13 10:35:57 -0400

    Add run.py; ignore its output
    Helper for run_tests that times out long runs.

---
+++ testing/run.py
@@ -0,0 +1,36 @@
+#! /usr/local/bin/python3
+""" Run the antler parser against a scribe block, and record
+    info about the block, as well as timing and output info
+    for later analysis.
+"""
+
+import subprocess
+import sys
+import time
+
+from pathlib import Path
+
+TIMELIMIT = 180
+
+block_type = sys.argv[1]
+test_dir = Path(f'./test_data.{block_type}')
+requirement_block = sys.argv[2]
+
+file = Path(test_dir, requirement_block)
+size = file.stat().st_size
+lines = file.read_text().count('\n')
+try:
+  t0 = time.time()
+  completed = subprocess.run(['grun', 'ReqBlock', 'req_block'],
+                             timeout=10,
+                             stdin=file.open(),
+                             capture_output=True)
+  elapsed = time.time() - t0
+  output = len(completed.stdout)
+
+except subprocess.TimeoutExpired:
+  output = 'timeout'
+  elapsed = TIMELIMIT + 1
+  print(f'{block_type}\t{requirement_block}\t{lines}\t{output}\tTIMEOUT', file=sys.stderr)
+
+print(f'{block_type}\t{requirement_block}\t{lines}\t{output}\t{elapsed:0.1f}')
