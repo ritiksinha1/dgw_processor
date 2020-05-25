566e4b72f7743e65e18dd9a0c31451c396868581
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-05-24 21:53:40 -0400

    Update dgw_filter.py
    Documentation.

--- dgw_filter.py
+++ dgw_filter.py
@@ -9,14 +9,20 @@ import sys
 
 # filter()
 # -------------------------------------------------------------------------------------------------
-def filter(src, remove_comments=False):
-  """ Remove all text following "END." as well as {hide } from src.
+def filter(src, remove_hide=True, remove_comments=False):
+  """ Remove all text following "END." Optionally, remove comments and text related to hiding
+      parts of the requirements from the audit report.
   """
+  # Remove all text following END.
   return_str = re.sub(r'[Ee][Nn][Dd]\.(.|\n)*', 'END.\n', src)
-  return_str = re.sub(r'[Hh][Ii][Dd][Ee]-?(([Ff][Rr][Oo][Mm]-?[Aa][Dd][Vv][Ii][Cc][Ee])|'
-                      r'([Rr][Uu][Ll][Ee]))?',
-                      '', return_str.replace('{', '').replace('}', ''))
 
+  # Remove {HIDE }, HIDE-FROM-ADVICE, and HIDE-RULE
+  if remove_hide:
+    return_str = re.sub(r'[Hh][Ii][Dd][Ee]-?(([Ff][Rr][Oo][Mm]-?[Aa][Dd][Vv][Ii][Cc][Ee])|'
+                        r'([Rr][Uu][Ll][Ee]))?',
+                        '', return_str.replace('{', '').replace('}', ''))
+
+  # Remove all comments
   if remove_comments:
     return_str = re.sub(f'#.*\n', '', return_str)
 
