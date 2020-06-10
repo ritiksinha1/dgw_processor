418dab200ac45c83c77b23f3ecbc8f0b2e2d2697
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-06-10 12:48:13 -0400

    Update run_tests and run.py
    Handle timelimit correctly.

--- testing/run.py
+++ testing/run.py
@@ -8,33 +8,37 @@ import os
 import subprocess
 import sys
 import time
-
+from argparse import ArgumentParser
 from pathlib import Path
 
-timelimit = 1500
+parser = ArgumentParser('run grun')
+parser.add_argument('block_type')
+parser.add_argument('requirement_block')
+parser.add_argument('-t', '--timelimit', default=900)
+args = parser.parse_args()
+timelimit = int(args.timelimit)
 
-block_type = sys.argv[1]
-test_dir = Path(f'./test_data.{block_type}')
-requirement_block = sys.argv[2]
+test_dir = Path(f'./test_data.{args.block_type}')
 
-file = Path(test_dir, requirement_block)
+file = Path(test_dir, args.requirement_block)
 size = file.stat().st_size
 lines = file.read_text().count('\n')
 try:
   t0 = time.time()
   completed = subprocess.run(['grun', 'ReqBlock', 'req_block'],
-                             # timeout=timelimit,
+                             timeout=timelimit,
                              stdin=file.open(),
                              capture_output=True)
   elapsed = time.time() - t0
   output = len(completed.stdout) + len(completed.stderr)
   if output != 0:
-    with open(f'test_results.{block_type}/{sys.argv[2]}', 'w') as errorlog:
+    with open(f'test_results.{args.block_type}/{args.requirement_block}', 'w') as errorlog:
       print(f'{completed.stdout} {completed.stderr}', file=errorlog)
 
 except subprocess.TimeoutExpired:
   output = 'timeout'
   elapsed = timelimit + 1
-  print(f'{block_type}\t{requirement_block}\t{lines}\t{output}\t{elapsed:0.1f}', file=sys.stderr)
+  print(f'{args.block_type}\t{args.requirement_block}\t{lines}\t{output}\t{elapsed:0.1f}',
+        file=sys.stderr)
 
-print(f'{block_type}\t{requirement_block}\t{lines}\t{output}\t{elapsed:0.1f}')
+print(f'{args.block_type}\t{args.requirement_block}\t{lines}\t{output}\t{elapsed:0.1f}')
