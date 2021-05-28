9df115e55b7719dcc296466a16a305ef5eaacc5e
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2021-05-27 23:01:19 -0400

    Add course_contexts.py
    Locate course lists and their contexts.

--- course_contexts.py
+++ course_contexts.py
@@ -5,15 +5,21 @@
 
 from argparse import ArgumentParser
 from pgconnection import PgConnection
+from dgw_interpreter import dgw_interpreter
+
+from pprint import pprint
 
 parser = ArgumentParser('Look up course list contexts')
 parser.add_argument('-i', '--institutions', nargs='*', default=['qns'])
 parser.add_argument('-t', '--block_types', nargs='*', default=['major'])
-parser.add_argument('-n', '--names', nargs='*', default=['all'])
+parser.add_argument('-v', '--block_values', nargs='*', default=['all'])
+parser.add_argument('-f', '--force', action='store_true')
 parser.add_argument('-p', '--period', default='current')
 args = parser.parse_args()
 period = args.period.lower()
-assert period in ['all', 'latest', 'current']
+
+# Allowable values for period
+assert period in ['all', 'current']
 
 conn = PgConnection()
 cursor = conn.cursor()
@@ -23,26 +29,39 @@ for institution in institutions:
   block_types = [arg.upper() for arg in args.block_types]
   for block_type in block_types:
     assert block_type in ['MAJOR', 'CONC', 'MINOR']
-    names = [name.lower() for name in args.names]
-    if 'all' in names:
+    block_values = [value.upper() for value in args.block_values]
+    if 'ALL' in block_values:
       cursor.execute(f"""
     select block_value
       from requirement_blocks
      where institution = '{institution}'
        and block_type = '{block_type}'
 """)
-      names = [row.block_value.lower() for row in cursor.fetchall()
-               if not row.block_value.isdigit()]
-    for name in names:
+      block_values = [row.block_value.upper() for row in cursor.fetchall()
+                      if not row.block_value.isdigit()
+                      and '?' not in row.block_value]
+    for block_value in block_values:
       cursor.execute(f"""
     select period_stop, header_list, body_list
       from requirement_blocks
      where institution = '{institution}'
        and block_type = '{block_type}'
-       and block_value ~* '^{name}$'
+       and block_value ~* '^{block_value}$'
 """)
       for row in cursor.fetchall():
-        print(cursor.query)
         if period == 'current' and row.period_stop != '99999999':
           continue
-        print(f'{institution} {block_type} {name} {period}')
+        print(f'{institution} {block_type} {block_value} {period}')
+        header_list, body_list = (row.header_list, row.body_list)
+        if len(header_list) == 0 or len(body_list) == 0 or args.force:
+          header_list, body_list = dgw_interpreter(institution, block_type, block_value, period)
+
+        # Find course lists and show their sizes and contexts (Don't get distracted by other stuff)
+        pprint(header_list)
+        pprint(body_list)
+        # for node in header_list:
+        #   for key in node.keys():
+        #     print('Header', key)
+        # for node in body_list:
+        #   for key in node.keys():
+        #     print('Body', key)
