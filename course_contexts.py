a1a495cab05f04152eb5cd54fd5d71eaf39bd9dc
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2021-07-05 23:29:22 -0400

    Update course_contexts.py
    Look at top-level dict keys.

--- course_contexts.py
+++ course_contexts.py
@@ -1,7 +1,10 @@
 #! /usr/local/bin/python3
 """ Look at course lists and their Scribe contexts.
     How to categorize courses as required, possible, forbidden?
+
 """
+
+import sys
 
 from argparse import ArgumentParser
 from collections import namedtuple
@@ -13,6 +16,21 @@ from inspect import currentframe, getframeinfo
 
 Course = namedtuple('Course',
                     'course_id offer_nbr discipline catalog_number title credits restriction')
+
+
+# Quarantined blocks
+quarantine_dict = {}
+with open('/Users/vickery/Projects/dgw_processor/testing/quarantine_list') as ql_file:
+  quarantine_list = ql_file.readlines()
+  for line in quarantine_list:
+    if line[0] == '#':
+      continue
+    body, ellucian = line.split('::')
+    ellucian = 'y' in ellucian or 'Y' in ellucian
+    college, requirement_id, *explanation = body.split(' ')
+    explanation = ' '.join(explanation).strip()
+
+    quarantine_dict[(college, requirement_id)] = (explanation.strip('.'), ellucian)
 
 
 # search_for()
@@ -106,7 +124,7 @@ if __name__ == '__main__':
   parser = ArgumentParser('Look up course list contexts')
   parser.add_argument('-i', '--institutions', nargs='*', default=['qns'])
   parser.add_argument('-t', '--block_types', nargs='*', default=['major'])
-  parser.add_argument('-v', '--block_values', nargs='*', default=['current'])
+  parser.add_argument('-v', '--block_values', nargs='*', default=['csci-ba'])
   parser.add_argument('-f', '--force', action='store_true')
   parser.add_argument('-p', '--period', default='current')
   args = parser.parse_args()
@@ -127,35 +145,51 @@ if __name__ == '__main__':
         block_values = [value.upper() for value in args.block_values]
         if 'ALL' in block_values:
           cursor.execute(f"""
-        select block_value
+        select distinct block_value
           from requirement_blocks
          where institution = '{institution}'
            and block_type = '{block_type}'
+        order by block_value
     """)
           block_values = [row.block_value.upper() for row in cursor.fetchall()
                           if not row.block_value.isdigit()
                           and '?' not in row.block_value]
         for block_value in block_values:
+          if block_value.startswith('MHC') or block_value.isdigit():
+            continue
           cursor.execute(f"""
-        select period_stop, requirement_text, header_list, body_list
+        select requirement_id, period_stop, requirement_text, header_list, body_list
           from requirement_blocks
          where institution = '{institution}'
            and block_type = '{block_type}'
            and block_value ~* '^{block_value}$'
     """)
           for row in cursor.fetchall():
-            if period == 'current' and row.period_stop != '99999999':
+            if (institution, row.requirement_id) in quarantine_dict:
+              print(f'{institution}, {row.requirement_id} is quarantined')
               continue
-            print(f'{institution} {block_type} {block_value} {period}')
+            if period == 'current' and row.period_stop != '99999999':
+              print(f'{institution}, {row.requirement_id} is not current')
+              continue
+            # print(f'{institution} {block_type} {block_value} {period}: ', end='')
             header_list, body_list = (row.header_list, row.body_list)
-            if len(header_list) == 0 or len(body_list) == 0 or args.force:
+            if (len(header_list) == 0 and len(body_list)) == 0 or args.force:
               header_list, body_list = dgw_interpreter(institution, block_type, block_value,
                                                        period_range=period)
 
+            print(f'*** {institution} {block_type} {block_value} {period}', file=debug)
             pprint(body_list, stream=debug)
+            print('\n', file=debug)
+
+            # What are the top-level rules(?) in the body?
+            for item in body_list:
+              keys = sorted(list(item.keys()))
+              print(f'{block_type} {keys}')
+            # print()
+
             # Find course lists in the body
-            current_path = [institution[0:3], block_type, block_value]
-            contexts = []
-            search_for(body_list, current_path, contexts)
-            for context in contexts:
-              print(context)
+            # current_path = [institution[0:3], block_type, block_value]
+            # contexts = []
+            # search_for(body_list, current_path, contexts)
+            # for context in contexts:
+            #   print(context)
