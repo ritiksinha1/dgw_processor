bc8bc106f8de93aa5def6ec63f99e02de7fd924e
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2021-08-04 11:10:13 -0400

    Add update_blocks.sh
    WIP

---
+++ testing/update_blocks.sh
@@ -0,0 +1,10 @@
+#! /usr/local/bin/bash
+
+# Generate Test Data
+# Check quarantined blocks for fixes
+# Run Tests
+# Investigate new timeouts and errors
+
+alias update_blocks=update_blocks.sh
+alias run_tests=run_tests.sh
+alias run_timeouts=run_timeouts.sh
