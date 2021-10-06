33e90583645ec5f69fdb1723ece782c596d9eafa
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2021-10-06 10:56:37 -0400

    Add run.sh and testing/rerun.sh
    Convenience scripts for re-parsing failing blocks.

---
+++ rerun.sh
@@ -0,0 +1,8 @@
+#! /usr/local/bin/bash
+
+dgw_parser.py -i BMC01 -ra RA000347
+dgw_parser.py -i HTR01 -ra RA002668
+dgw_parser.py -i HTR01 -ra RA002670
+dgw_parser.py -i HTR01 -ra RA002647
+dgw_parser.py -i MEC01 -ra RA001497
+dgw_parser.py -i MEC01 -ra RA001498
