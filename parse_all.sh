80bade1cbc7bf2de3486d11e6fc4f245369e7c28
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2021-11-14 10:40:04 -0500

    Update parse_all.sh
    Comment change

--- parse_all.sh
+++ parse_all.sh
@@ -1,6 +1,6 @@
 #! /usr/local/bin/bash
 
-# Parse all blocks for all institutions with option to ignore institutions
+# Parse all current blocks for all institutions with option to ignore institutions
 institutions=(bar bcc bkl bmc csi cty grd hos htr jjc kcc lag law leh mec med ncc nyt qcc qns slu \
               soj sph sps yrk)
 
