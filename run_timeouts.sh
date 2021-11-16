ee6f0e64e6c477d89ea07976e534181ca6c87d1f
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2021-11-16 08:34:53 -0500

    Update run_timeouts.sh
    Fix bug

--- run_timeouts.sh
+++ run_timeouts.sh
@@ -7,31 +7,20 @@ then  echo Usage: run_timeouts.sh [file]
       exit 1
 fi
 
-if [[ !($1 =~ err$) ]]
-then read -p "Are you sure $1 is an err file [yN]? "
-     [[ !($REPLY =~ [Yy]) ]] && exit
-fi
-
 timeout=1800
 [[ $TIMEOUT_INTERVAL != '' ]] && timeout=$TIMEOUT_INTERVAL
 
 echo "Timeout Interval is $timeout seconds"
 
-read -p 'Truncate out and err [Yn]? '
-if [[ $REPLY =~ [Nn] ]]
-then echo Not truncated
-else truncate -s 0 run_timeouts.out run_timeouts.err
-fi
-
 SECONDS=0
-while read institution requirement_id reason remainder < $1
+while read institution requirement_id reason remainder
 do
   if [[ ${reason} == Timeout ]]
   then
     institution=`echo $institution| cut -c 1-3 | tr A-Z a-z`
     requirement_id=`echo $requirement_id |cut -c 3-8`
-    dgw_parser.py -i ${institution} -ra ${requirement_id} -ti ${timeout} >>run_timeouts.out 2>>run_timeouts.err
+    dgw_parser.py -i ${institution} -ra ${requirement_id} -ti ${timeout}
   fi
-done
+done < $1
 
 echo "That took $SECONDS seconds"
