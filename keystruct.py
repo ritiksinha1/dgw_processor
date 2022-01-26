06e4a797d5c70368792df42c407dfe3d49bdffbc
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-01-25 23:21:34 -0500

    Collect min_gpa from program header info.

--- keystruct.py
+++ keystruct.py
@@ -2,18 +2,32 @@
 """ List program requirements and courses that satisfy them.
 """
 
+import csv
 import os
 import psycopg
 import sys
+
+from typing import Any
 from argparse import ArgumentParser
 from collections import namedtuple, defaultdict
 from recordclass import recordclass
 from psycopg.rows import namedtuple_row
 
 BlockInfo = recordclass('BlockInfo', 'institution requirement_id block_type block_value title '
-                        'parse_tree class_credits max_transfer min_grade')
+                        'parse_tree class_credits max_transfer min_grade min_gpa')
+
 
 log_file = open(f'{__file__.replace(".py", ".log")}', 'w')
+csv_file = open(f'{__file__.replace(".py", ".csv")}', 'w')
+
+
+def dict_factory():
+  """ Support for three index levels, as in courses_by_institution.
+  """
+  return defaultdict(dict)
+
+
+courses_by_institution = defaultdict(dict_factory)
 
 
 # letter_grade()
@@ -47,7 +61,7 @@ def letter_grade(grade_point: float) -> str:
 # write_log()
 # -------------------------------------------------------------------------------------------------
 def write_log(block_info: namedtuple, message: str):
-  """ Write a message to the logfile
+  """ Write a message to the logfile.
   """
   print(f'{block_info.institution} {block_info.requirement_id} {block_info.block_type:6} '
         f'{block_info.block_value:10}', message, file=log_file)
@@ -82,23 +96,33 @@ def key_struct(arg, depth=0):
 
 # traverse_body()
 # -------------------------------------------------------------------------------------------------
-def traverse_body(block_info: namedtuple, context: list) -> None:
+def traverse_body(node: Any, context: list) -> None:
   """ Extract Requirement names and course lists from body rules.
+      Element 0 of the context list is always information about the block, including header
+      restrictions: MaxTransfer and MinGrade. The context list is augmented with requirement names
+      (labels), conditions, and residency/grade requirements during recursion.
+        body_rule       : block
+                        | blocktype
+                        | body_class_credit
+                        | body_conditional
+                        | course_list_rule
+                        | copy_rules
+                        | group_requirement
+                        | noncourse
+                        | proxy_advice
+                        | remark
+                        | rule_complete
+                        | subset
   """
 
-  try:
-    body_list = block_info.parse_tree['body_list']
-  except KeyError:
-    write_log(block_info, f'No body list in parse tree')
-    return
+  if isinstance(node, list):
+    for item in node:
+      pass
 
-  for body_item in body_list:
-    if isinstance(body_item, list):
-      pass
-    elif isinstance(body_item, dict):
-      pass
-    else:
-      pass
+  elif isinstance(node, dict):
+    pass
+  else:
+    pass
 
 
 # traverse_header()
@@ -123,25 +147,27 @@ def traverse_header(block_info: namedtuple) -> None:
             if label_str := value['label']:
               write_log(block_info, f'Class/Credit label: {label_str}')
 
-            if value['min_classes'] is None and value['max_classes'] is None:
-              if value['min_credits'] == value['max_credits']:
-                class_credits = float(value['max_credits'])
-                block_info.class_credits = f'{class_credits:5.1f} Credits'
+            min_classes = None if value['min_classes'] is None else int(value['min_classes'])
+            min_credits = None if value['min_credits'] is None else float(value['min_credits'])
+            max_classes = None if value['max_classes'] is None else int(value['max_classes'])
+            max_credits = None if value['max_credits'] is None else float(value['max_credits'])
+            assert not (min_credits and max_credits is None), f'{min_credits} {max_credits}'
+            assert not (min_credits is None and max_credits), f'{min_credits} {max_credits}'
+            assert not (min_classes and max_classes is None), f'{min_classes} {max_classes}'
+            assert not (min_classes is None and max_classes), f'{min_classes} {max_classes}'
+            class_credit_list = []
+            if min_classes and max_classes:
+              if min_classes == max_classes:
+                class_credit_list.append(f'{max_classes} classes')
               else:
-                min_credits = float(value['min_credits'])
-                max_credits = float(value['max_credits'])
-                block_info.class_credits = f'{min_credits:5.1f}-{max_credits:4.1f} Credits'
-            elif value['min_credits'] is None and value['max_credits'] is None:
-              if value['min_classes'] == value['max_classes']:
-                class_credits = int(value['max_classes'])
-                block_info.class_credits = f'{class_credits:5} Classes'
+                class_credit_list.append(f'{min_classes}-{max_classes} classes')
+
+            if min_credits and max_credits:
+              if min_credits == max_credits:
+                class_credit_list.append(f'{max_credits:.1f} credits')
               else:
-                min_classes = int(value['min_classes'])
-                max_classes = int(value['max_classes'])
-                block_info.class_credits = f'{min_classes:5}-{max_classes} Classes'
-            else:
-              # *** THERE IS AN AND LIST WITH BOTH CLASSES AND CREDITS ***
-              write_log(block_info, f'Class/Credit: {value}')
+                class_credit_list.append(f'{min_credits:.1f}-{max_credits:.1f} credits')
+            block_info.class_credits = ' and '.join(class_credit_list)
 
           case 'conditional':
             pass
@@ -163,18 +189,20 @@ def traverse_header(block_info: namedtuple) -> None:
           case 'header_maxperdisc':
             # THERE WOULD BE A COURSE LIST HERE
             pass
+
           case 'header_maxterm':
             pass
+
           case 'header_maxtransfer':
             if label_str := value['label']:
               write_log(block_info, f'MaxTransfer label: {label_str}')
             number = float(value['maxtransfer']['number'])
             class_or_credit = value['maxtransfer']['class_or_credit']
             if class_or_credit == 'credit':
-              block_info.max_transfer = f'{number:0.1f} credits'
+              block_info.max_transfer = f'{number:3.1f} credits'
             else:
               suffix = '' if int(number) == 1 else 'es'
-              block_info.max_transfer = f'{int(number)} class{suffix}'
+              block_info.max_transfer = f'{int(number):3} class{suffix}'
 
           case 'header_minclass':
             # THERE WOULD BE A COURSE LIST HERE
@@ -182,12 +210,16 @@ def traverse_header(block_info: namedtuple) -> None:
           case 'header_mincredit':
             # THERE WOULD BE A COURSE LIST HERE
             pass
+
           case 'header_mingpa':
-            pass
+            if label_str := value['label']:
+              write_log(block_info, f'MinGPA label: {label_str}')
+            mingpa = float(value['mingpa']['number'])
+            block_info.min_gpa = f'{mingpa:4.2f}'
+
           case 'header_mingrade':
             if label_str := value['label']:
               write_log(block_info, f'MinGrade label: {label_str}')
-
             block_info.min_grade = letter_grade(float(value['mingrade']['number']))
 
           case 'header_minperdisc':
@@ -231,6 +263,15 @@ if __name__ == "__main__":
 
   empty_tree = "'{}'"
 
+  writer = csv.writer(csv_file)
+  writer.writerow(['College',
+                   'Requirement ID',
+                   'Type',
+                   'Code',
+                   'Total',
+                   'Max Transfer',
+                   'Min Grade',
+                   'Min GPA'])
   with psycopg.connect('dbname=cuny_curriculum') as conn:
     with conn.cursor(row_factory=namedtuple_row) as cursor:
       if args.institution.upper() == 'ALL':
@@ -285,16 +326,43 @@ if __name__ == "__main__":
       suffix = '' if cursor.rowcount == 1 else 's'
       print(f'{cursor.rowcount} parse tree{suffix}')
       for row in cursor:
-        # Augment db info with default values for total credits, max transfer, and min grade
-        block_info = BlockInfo._make(row + (' ----        ', 'No Limit', 'Any'))
+        # If this is the first time this instution has been encountered, create a dict mapping
+        # courses to their course_id:offer_nbr values
+        if row.institution not in courses_by_institution:
+          with conn.cursor(row_factory=namedtuple_row) as course_cursor:
+            course_cursor.execute("""
+            select institution, course_id, offer_nbr,discipline, catalog_number, career
+              from cuny_courses
+             where institution = %s
+               and designation not in ('MNL', 'MLA')
+               and course_status = 'A'
+               and attributes !~* 'BKCR'
+             order by discipline, numeric_part(catalog_number)
+            """, (row.institution, ))
+            for course in course_cursor:
+              value = (course.course_id, course.offer_nbr, course.career)
+              courses_by_institution[course.institution][course.discipline][course.catalog_number] = value
+
+        # Augment db info with default values for class_credits max_transfer min_grade min_gpa
+        block_info = BlockInfo._make(row + ('', '', '', ''))
         try:
           traverse_header(block_info)
-          print(f'{block_info.institution[0:3]} {block_info.requirement_id} '
-                f'{block_info.block_type:6} {block_info.block_value:10} {block_info.class_credits};'
-                f' Min Grade: {block_info.min_grade:3}; Max Transfer: '
-                f'{block_info.max_transfer}')
-          traverse_body(block_info, [])
+
+          writer.writerow([f'{block_info.institution[0:3]}',
+                           f'{block_info.requirement_id}',
+                           f'{block_info.block_type}',
+                           f'{block_info.block_value}',
+                           f'{block_info.class_credits}',
+                           f'{block_info.max_transfer}',
+                           f'{block_info.min_grade}',
+                           f'{block_info.min_gpa}'])
+
         except KeyError:
           print(f'No header_list for', institution, requirement_id, block_type, block_value,
                 title, file=sys.stderr)
-          pass
+        try:
+          body_list = block_info.parse_tree['body_list']
+          traverse_body(body_list, [block_info])
+        except KeyError:
+          print(f'No body_list for', institution, requirement_id, block_type, block_value,
+                title, file=sys.stderr)
