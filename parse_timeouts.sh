72b10a08524852422db85f7c3feaae0749d43f1e
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-01-31 16:35:06 -0500

    Clean up parse_timeouts.sh

--- parse_timeouts.sh
+++ parse_timeouts.sh
@@ -3,7 +3,7 @@
 # Given an error file, pick out the Timeout lines, and re-parse them with a longer timeout interval
 
 if [[ $# != 1 ]]
-then  echo Usage: run_timeouts.sh [file]
+then  echo "Usage: $0 [file]"
       exit 1
 fi
 
@@ -18,7 +18,8 @@ let $(( num_done = 0 ))
 timeout=1800
 [[ $TIMEOUT_INTERVAL != '' ]] && timeout=$TIMEOUT_INTERVAL
 
-echo "Timeout Interval is $timeout seconds" > $log_file
+# Initialize log_file and err_file
+echo "Timeout Interval is $timeout seconds" >$log_file 2>$err_file
 
 SECONDS=0
 max_time=0
@@ -31,11 +32,11 @@ do
     institution=`echo $institution| cut -c 1-3 | tr A-Z a-z`
     requirement_id=`echo $requirement_id |cut -c 3-8`
     echo -ne "\r$num_done / $num_todo $institution $requirement_id "
-    dgw_parse.py -i ${institution} -ra ${requirement_id} -ti ${timeout} >> $log_file 2>$err_file
+    dgw_parser.py -i ${institution} -ra ${requirement_id} -ti ${timeout} >> $log_file 2>$err_file
     elapsed=$(( $SECONDS - $start ))
     [[ $(( $elapsed > $max_time )) ]] && max_time=$elapsed
     echo -n "$elapsed sec."
   fi
 done < $1
 
-echo -e "\nThat took $SECONDS seconds; max was $max_time sec"  > $log_file
+echo -e "\nThat took $SECONDS seconds; max was $max_time sec."  > $log_file
