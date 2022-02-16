c6e646cb07b1786b1600d890c697155f64dd7e41
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-02-16 14:38:15 -0500

    Add class for indicating Scribe Errors

---
+++ scriberror.py
@@ -0,0 +1,8 @@
+""" Separate module for empty class to avoid circular module dependencies.
+"""
+
+
+# Class ScribeError()
+# -------------------------------------------------------------------------------------------------
+class ScribeError(Exception):
+  pass
