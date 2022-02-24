c96c374ef7129f7d0e5f2fd29dc7b00e37da87a3
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-02-23 20:25:29 -0500

    Improvement!

--- reports.sh
+++ reports.sh
@@ -1,7 +1,7 @@
 #! /usr/local/bin/bash
 
 echo `date` > reports.txt
-for r in debug log todo fail
+for r in log todo fail debug
 do
     echo $r | tr a-z A-Z >> reports.txt
     cut -c 16- course_mapper.${r}.txt|sort|uniq -c|sort -r >> reports.txt
