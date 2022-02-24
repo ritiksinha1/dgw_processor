f25d89f11446ff00675d4b5278aec08fc039d88e
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-02-23 20:22:34 -0500

    Expand report types

--- reports.sh
+++ reports.sh
@@ -1,7 +1,7 @@
 #! /usr/local/bin/bash
 
 echo `date` > reports.txt
-for r in debug log fail
+for r in debug log todo fail
 do
     echo $r | tr a-z A-Z >> reports.txt
     cut -c 16- course_mapper.${r}.txt|sort|uniq -c|sort -r >> reports.txt
