3a692a68934c7d70cc3ff0ececdef5f984c2e297
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-04-03 11:18:15 -0400

    Development setup cleanup

--- reports.sh
+++ reports.sh
@@ -4,5 +4,5 @@ echo `date` > reports.txt
 for r in log todo fail debug no_courses
 do
     echo $r | tr a-z A-Z >> reports.txt
-    cut -c 16- course_mapper.${r}.txt|sort|uniq -c|sort -r >> reports.txt
+    cut -c 16- ${r}.txt|sort|uniq -c|sort -r >> reports.txt
 done
