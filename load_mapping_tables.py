664bfc2f46ec7806de89d301c430999231fe164c
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-06-07 18:54:45 -0400

    Load large mapping tables w/ progress

--- load_mapping_tables.py
+++ load_mapping_tables.py
@@ -12,19 +12,33 @@ from pathlib import Path
 from psycopg.rows import namedtuple_row
 from time import time
 
+
+def _count_generator(reader):
+    b = reader(1024 * 1024)
+    while b:
+        yield b
+        b = reader(1024 * 1024)
+
+
 if __name__ == '__main__':
   session_start = time()
+  csv.field_size_limit(sys.maxsize)
   with psycopg.connect('dbname=course_mappings') as conn:
     with conn.cursor(row_factory=namedtuple_row) as cursor:
 
       csv_files = Path('.').glob('c*v')
       for file in csv_files:
-        table_name = file.name.replace('course_mapper.', '').replace('.csv', '')
 
-        print(table_name)
+        with open(file, 'rb') as fp:
+          c_generator = _count_generator(fp.raw.read)
+          num_lines = sum(buffer.count(b'\n') for buffer in c_generator)
+        table_name = file.name.replace('course_mapper.', '').replace('.csv', '')
+        print(f'\n{table_name}: {num_lines:,} lines')
+        nl = num_lines / 100.0
         with open(file) as csv_file:
           reader = csv.reader(csv_file)
           for line in reader:
+            print(f'\r{reader.line_num:,}/{num_lines:,} {round(reader.line_num/nl)}%', end='')
             if reader.line_num == 1:
               Row = namedtuple('Row', [col.lower().replace(' ', '_').replace('with', 'with_exp')
                                for col in line])
@@ -42,4 +56,4 @@ if __name__ == '__main__':
               cursor.execute(f'insert into {table_name} values({values})')
   min, sec = divmod(time() - session_start, 60)
   hr, min = divmod(min, 60)
-  print(f'Total {int(hr):02}:{int(min):02}:{round(sec):02}')
+  print(f'\nTotal {int(hr):02}:{int(min):02}:{round(sec):02}')
