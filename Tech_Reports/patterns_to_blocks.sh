96b03e3df23c5194a2db00fe2f1961b16710e7dd
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-06-08 22:23:59 -0400

    Update Tech Report scripts

--- Tech_Reports/patterns_to_blocks.sh
+++ Tech_Reports/patterns_to_blocks.sh
@@ -1,9 +1,18 @@
 #! /usr/local/bin/bash
 
-tail -n+2 header_body_summary.csv |cut -c 10- > patterns
+# For each pattern in header_body_summary.csv, list the blocks where it occurred.
+# Output is in the same order as the in the csv file: decreasing frequency with which the pattern
+# occurred.
+(
+  cd /Users/vickery/Projects/dgw_processor/Tech_Reports
 
-while read
-do echo $REPLY
-   ack "$REPLY" header_body_report.txt | cut -c 1-14|sort|uniq
-done <patterns
-rm patterns
+  # Extract the patterns found in header_body_summary, without the frequencies at the beginning
+  tail -n+2 header_body_summary.csv |cut -c 10- > patterns
+
+  # For each pattern, print the pattern, followed by the block(s) where it was found.
+  while read
+  do echo $REPLY|tr -d ' '|tr ',' '-'
+    ack "$REPLY" header_body_report.txt | cut -c 1-14|sort|uniq|sed "s/^/  /"
+  done <patterns
+  rm patterns
+)
--- Tech_Reports/summarize_report.sh
+++ Tech_Reports/summarize_report.sh
@@ -7,14 +7,20 @@
 
 (
  cd /Users/vickery/Projects/dgw_processor/Tech_Reports
-
+SECONDS=0
 # Save the CSV header row from the report
 head -1 header_body_report.txt > header_row
 
 # Show number of blocks containing potential redundancies
+echo Generate header_body_summary.csv
 tail -n+2 header_body_report.txt | ack 'CROSS|EQUIV|MATCH' | cut -c 37- | sort | uniq -c | sort -r
 tail -n+2 header_body_report.txt | ack -v 'CROSS|EQUIV|MATCH' | cut -c 24- | sort | uniq -c | \
   sort -r > body_rows
 cat header_row body_rows > header_body_summary.csv
 rm -f header_row body_rows
+echo $SECONDS sec.
+SECONDS=0
+echo -e "\nGenerate patterns_to_blocks.txt"
+patterns_to_blocks.sh > patterns_to_blocks.txt
+echo $SECONDS sec.
 )
