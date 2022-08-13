7cc2f57395b9388a6d6564dada0f157ca4857689
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-08-13 10:14:44 -0400

    Improve reports structure

--- reports.sh
+++ reports.sh
@@ -5,8 +5,13 @@ cat blocks.txt|cut -c 1-3,15-|sort|uniq -c >> reports.txt
 n=`wc -l blocks.txt`
 echo "   ${n/.txt/}" >> reports.txt
 
-for r in log todo fail debug analysis no_courses
+for r in log todo fail no_courses debug analysis
 do
     echo -e "\n$r" | tr a-z A-Z >> reports.txt
-    cut -c 16- ${r}.txt|sort|uniq -c|sort -r >> reports.txt
+    if [[ $r = no_courses ]]
+    then
+        cat ${r}.txt|sort|uniq -c|sort -r >> reports.txt
+    else
+        cut -c 16- ${r}.txt|sort|uniq -c|sort -r >> reports.txt
+    fi
 done
