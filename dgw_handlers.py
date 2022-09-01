89f13369c1e51d0ab3242a95dbf58e8c925a4013
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-08-31 23:55:38 -0400

    Subset course_list_rule fix-up.

--- dgw_handlers.py
+++ dgw_handlers.py
@@ -327,7 +327,7 @@ def copy_rules(ctx, institution, requirement_id):
       if cursor.rowcount == 0:
         return_dict['error'] = (f'“Missing {requirement_id}” for {institution}')
       else:
-        row = cursor.fetchone()  # There cannot be mnore than one requrement block per requirement id
+        row = cursor.fetchone()  # There cannot be mnore than one block per requirement id
         block_type = row.block_type
         block_value = row.block_value
         if row.period_stop.startswith('9'):
@@ -1173,9 +1173,12 @@ def subset(ctx, institution, requirement_id):
     return_dict.update(copy_rules(ctx.copy_rules()[0], institution, requirement_id))
 
   if ctx.course_list_rule():
-    return_dict['course_lists'] = [build_course_list(context.course_list_body().course_list(),
-                                                     institution, requirement_id)
-                                   for context in ctx.course_list_rule()]
+    # The grammar makes this a list, but the length will always be unity
+    contexts = [context for context in ctx.course_list_rule()]
+    assert len(contexts) == 1, f'Multiple ({len(contexts)} contexts in subset course_list_rule)'
+    return_dict.update({'course_list_rule':
+                        build_course_list(contexts[0].course_list_body().course_list(),
+                                          institution, requirement_id)})
 
   if ctx.group_requirement():
     return_dict.update(group_requirement(ctx.group_requirement(), institution, requirement_id))
