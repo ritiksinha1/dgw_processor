c563ffb9a1ea149d3259719f007d556218f0eb02
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-10-07 23:17:24 -0400

    Add active_plans/subplans to active_blocks

--- activeblocks.py
+++ activeblocks.py
@@ -8,20 +8,25 @@ import sys
 from psycopg.rows import namedtuple_row
 
 active_blocks = dict()
+active_plans = dict()
+active_subplans = dict()
 
 with psycopg.connect('dbname=cuny_curriculum') as conn:
   with conn.cursor(row_factory=namedtuple_row) as cursor:
     cursor.execute("""
-    select institution, requirement_id,
+    select institution, requirement_id, block_type, block_value,
            min(active_term) as min_term,
            max(active_term) as max_term,
            count(active_term) as num_terms,
            sum(total_students) as enrollment
       from ra_counts
-    -- where institution = 'LEH01' and requirement_id in ('RA001829', 'RA002612', 'RA002360')
-    group by institution, requirement_id
-    order by institution, requirement_id
+    group by institution, requirement_id, block_type, block_value
+    order by institution, requirement_id, block_type, block_value
     ;
     """)
     for row in cursor:
       active_blocks[(row.institution, row.requirement_id)] = row
+      if row.block_type in ['MAJOR', 'MINOR']:
+        active_plans[(row.institution, row.block_value)] = row
+      if row. block_type == 'CONC':
+        active_subplans[(row.institution, row.block_value)] = row
