3e841500c139d7254543741a22d5589444700629
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-10-10 11:26:10 -0400

    Include block title & major1 values from ra_counts

--- activeblocks.py
+++ activeblocks.py
@@ -1,5 +1,21 @@
 #! /usr/local/bin/python3
-"""
+""" Scribe blocks can be "current," but the underlying program or subprogram might no longer be
+    "active" (accepting students).
+
+    A "current" dap_req_block ("Scribe Block" or "requirement_block") is one that has a period_stop
+    value that begins with '9'. However, programs can become inactive (no longer offered), without
+    anyone updating the period_stop field for the block. The CUNYfirst acad_plan and acad_subplan
+    tables tell what programs/concentrations are "active," but this doesn't necessarily mean the
+    program or concentration is currently being offered. There is a pair of CUNYfirst tables that
+    give the current enrollment for each plan/subplan, but we have seen instances where
+    known-inactive programs show up with non-zero enrollments.
+
+    OAREDA supplies a list of dap_req_blocks that are "truly active" in a file called
+    dgw_ir_active_requirements.csv, which is updated daily. A script called mk_ra_counts builds a
+    local table called ra_counts from the OAREDA CSV file. This code joins information from
+    ra_counts with information from the OAREDA dap_req_block table (specifically, an augemented
+    local copy called requirement_blocks) to produce dicts for access that information by block,
+    plan, and subplan.
 """
 
 import psycopg
@@ -14,13 +30,13 @@ active_subplans = dict()
 with psycopg.connect('dbname=cuny_curriculum') as conn:
   with conn.cursor(row_factory=namedtuple_row) as cursor:
     cursor.execute("""
-    select institution, requirement_id, block_type, block_value,
+    select institution, requirement_id, block_type, block_value, block_title, major1,
            min(active_term) as min_term,
            max(active_term) as max_term,
            count(active_term) as num_terms,
            sum(total_students) as enrollment
       from ra_counts
-    group by institution, requirement_id, block_type, block_value
+    group by institution, requirement_id, block_type, block_value, block_title, major1
     order by institution, requirement_id, block_type, block_value
     ;
     """)
