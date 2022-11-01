50c09f93a9b6cee886704abdd81b8bebda5174e2
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-11-01 10:29:33 -0400

    Show progress is optional (again)

--- parse_quarantined.py
+++ parse_quarantined.py
@@ -2,6 +2,7 @@
 """ Parse all currently-quarantined blocks. Liberate any that now parse okay.
 """
 
+import argparse
 import os
 import psycopg
 import sys
@@ -11,59 +12,67 @@ from psycopg.rows import namedtuple_row
 from quarantine_manager import QuarantineManager
 
 
-quarantined_dict = QuarantineManager()
+if __name__ == '__main__':
+  # Do you really need to show progress?
+  # You are going to see ANTLR/DGW, etc messages for each file anyway.
+  parser = argparse.ArgumentParser('Try to re-parse quarantined dap_req_blocks')
+  parser.add_argument('-p', '--show-progress', action='store_true')
+  args = parser.parse_args()
 
-# Convert keys to a list so we can delete from the dict if anything parses correctly now.
-keys = list(quarantined_dict.keys())
-num_quarantined = len(keys)
-num_nolonger = 0
-num_success = 0
-num_fail = 0
-error_word = '{error}'
-with psycopg.connect('dbname=cuny_curriculum') as conn:
-  with conn.cursor(row_factory=namedtuple_row) as cursor:
-    with conn.cursor(row_factory=namedtuple_row) as update_cursor:
-      for index, key in enumerate(keys):
-        institution, requirement_id = key
-        print(f'{institution} {requirement_id} {index+1:3}/{len(keys)}')
+  quarantined_dict = QuarantineManager()
 
-        cursor.execute("""
-        select institution, requirement_id, period_start, period_stop,
-               requirement_text,
-               parse_tree->'error' as analysis_text
-          from requirement_blocks
-         where institution = %s
-           and requirement_id = %s
-        """, (institution, requirement_id))
-        row = cursor.fetchone()
-        if not row.period_stop.startswith('9'):
-          num_nolonger += 1
-          del quarantined_dict[key]
-          print(f'{institution} {requirement_id} Dequarantined: No longer current')
-          continue
-        parse_tree = parse_block(institution, requirement_id, row.period_start, row.period_stop,
-                                 row.requirement_text)
-        try:
-          if error_msg := parse_tree['error']:
-            num_fail += 1
-            if 'timeout' in error_msg.lower():
-              print(f'{institution} {requirement_id} Timeout: {error_msg}')
+  # Convert keys to a list so we can delete from the dict if anything parses correctly now.
+  keys = list(quarantined_dict.keys())
+  num_quarantined = len(keys)
+  num_nolonger = 0
+  num_success = 0
+  num_fail = 0
+  error_word = '{error}'
+  with psycopg.connect('dbname=cuny_curriculum') as conn:
+    with conn.cursor(row_factory=namedtuple_row) as cursor:
+      with conn.cursor(row_factory=namedtuple_row) as update_cursor:
+        for index, key in enumerate(keys):
+          institution, requirement_id = key
+          if args.show_progress:
+            print(f'{institution} {requirement_id} {index+1:3}/{len(keys)}')
 
-            # Restore the original error message
-            restore_query = f"""
-            update requirement_blocks
-                   set parse_tree = jsonb_set(parse_tree, '{error_word}', '"{row.analysis_text}"')
-             where institution = '{institution}'
-               and requirement_id = '{requirement_id}'"""
-            update_cursor.execute(restore_query)
-        except KeyError:
-          # Parsed okay: dequarantine it.
-          del quarantined_dict[key]
-          print(f'{institution} {requirement_id} Parsed OK: No longer quarantined')
-          num_success += 1
+          cursor.execute("""
+          select institution, requirement_id, period_start, period_stop,
+                 requirement_text,
+                 parse_tree->'error' as analysis_text
+            from requirement_blocks
+           where institution = %s
+             and requirement_id = %s
+          """, (institution, requirement_id))
+          row = cursor.fetchone()
+          if not row.period_stop.startswith('9'):
+            num_nolonger += 1
+            del quarantined_dict[key]
+            print(f'{institution} {requirement_id} Dequarantined: No longer current')
+            continue
+          parse_tree = parse_block(institution, requirement_id, row.period_start, row.period_stop,
+                                   row.requirement_text)
+          try:
+            if error_msg := parse_tree['error']:
+              num_fail += 1
+              if 'timeout' in error_msg.lower():
+                print(f'{institution} {requirement_id} Timeout: {error_msg}')
 
-print()
-print(f'Quarantined before:  {num_quarantined:>5,}\n'
-      f'No longer current:   {num_nolonger:>5}\n'
-      f'Parsed successfully: {num_success:>5,}\n'
-      f'Quarantined after:   {num_fail:>5,}')
+              # Restore the original error message
+              restore_query = f"""
+              update requirement_blocks
+                     set parse_tree = jsonb_set(parse_tree, '{error_word}', '"{row.analysis_text}"')
+               where institution = '{institution}'
+                 and requirement_id = '{requirement_id}'"""
+              update_cursor.execute(restore_query)
+          except KeyError:
+            # Parsed okay: dequarantine it.
+            del quarantined_dict[key]
+            print(f'{institution} {requirement_id} Parsed OK: No longer quarantined')
+            num_success += 1
+
+  print()
+  print(f'Quarantined before:  {num_quarantined:>5,}\n'
+        f'No longer current:   {num_nolonger:>5}\n'
+        f'Parsed successfully: {num_success:>5,}\n'
+        f'Quarantined after:   {num_fail:>5,}')
