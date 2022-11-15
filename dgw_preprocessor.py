195343780c960cdb43bf1c8a1d591bcba161914b
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-11-15 00:28:16 -0500

    Preprocessor: handle bangs and pounds in strings

--- dgw_preprocessor.py
+++ dgw_preprocessor.py
@@ -9,10 +9,14 @@ import sys
 
 # dgw_filter()
 # -------------------------------------------------------------------------------------------------
-def dgw_filter(src, remove_hide=True, remove_comments=False):
-  """ Remove all text following "END." Optionally, remove comments and text related to hiding
-      parts of the requirements from the audit report.
+def dgw_filter(src, remove_comments=True, remove_hide=False):
+  """ Remove all text following "END."
+      Straighten left curly single quotes. # TESTING: is this necessary?
+      Optional (default True): remove all comments.
+      Optional (default False):remove content inside hide-from-advice structures. Default is to
+                remove all curly braces and hide-from-advice keywords.
   """
+
   # Remove all text following END.
   return_str = re.sub(r'[Ee][Nn][Dd]\.(.|\n)*', 'END.\n', src)
 
@@ -21,13 +25,60 @@ def dgw_filter(src, remove_hide=True, remove_comments=False):
 
   # Remove comments
   if remove_comments:
-    return_str = re.sub(f'#.*\n', '', return_str)
+    # Protect strings that contain #'s and/or !'s
+    while re.search(r'".*?#|!.*?"', return_str):
+      return_str = re.sub(r'(.*".*?)#|!(.*?".*)', r'\1VICKERY\2', return_str)
 
-  # Remove {HIDE <hidden_content>}, HIDE-FROM-ADVICE, and HIDE-RULE, but leave <hidden_content>
+    return_str = re.sub(f'#.*?\n', '\n', return_str)
+
+    # Restore strings
+    return_str = re.sub('VICKERY', '#', return_str)
+
+  # Hide hide-rule to avoid confound with hide(-from-advice)? keyword
+  return_str = re.sub(r'hide-?rule', 'VICKERY', return_str, flags=re.I)
+
   if remove_hide:
-    return_str = re.sub(r'[Hh][Ii][Dd][Ee]-?(([Ff][Rr][Oo][Mm]-?[Aa][Dd][Vv][Ii][Cc][Ee])|'
-                        r'([Rr][Uu][Ll][Ee]))?',
-                        '', return_str.replace('{', '').replace('}', ''))
+    # Remove {HIDE-FROM-ADVICE <hidden_content>}
+    """
+        From Scribe Language Users Guide:
+        The trick to knowing where the place the commas when using Hide (from advice) is to be sure
+        that everything within and including the braces can be removed leaving a valid, parseable
+        course rule.
+        # Hide ECON 112 from the advice
+         3 Classes in ACCT 103, 105, {Hide ECON 112,} 114
+          Label ACCOUNTING "Accounting Requirements";
+        # Hide MATH 104 from the advice
+         3 Credits in MATH 184, {HideFromAdvice MATH 104 (WITH DWTerm < 201230)}
+           Label MATH "Math Requirement";
+        # Show HIST 184 but hide the WITH information
+         5 Credits in HIST 184 (WITH Hide DWResident = Y)
+           Label HIST "History Requirement";
+
+        But Note that the hide clause can be inside a with-clause or group enclosed in parentheses
+        instead of curly quotes, or an area enclosed in square brackets.
+
+        Other observation: if all the courses in an area are hidden, you can end up with an empty
+        area. Likewise, you can end up with a class/credit requirement where the course list ends up
+        missing. And handling hidden groups means dealing with OR before, after, or neither of the
+        parens.
+
+        Further note, that a course can satisfy a requirement even if it is hidden, so the mapper
+        should see them anyway.
+
+        So this feature is not implemented at this time
+  """
+    exit('dgw_filter: remove_hide option not supported at this time.')
+
+  else:
+    # remove all curly braces and hide-from-advice keywords
+    return_str = return_str.replace('{', '').replace('}', '')
+    # Note that this also removes "hide" from proxy-advice and remark strings.
+    # https://www.thefreedictionary.com/words-containing-hide suggests that this will not be an
+    # issue.
+    return_str = re.sub(r'hide(-?from-?advice)?', '', return_str, flags=re.I | re.S)
+
+    # Restore any omitted Hide-Rule keywords
+    return_str = re.sub('VICKERY', 'Hide-Rule', return_str)
 
   return return_str
 
